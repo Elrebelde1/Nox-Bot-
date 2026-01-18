4d20d5c395c2699ba7e1d3456fb668635f19d238
Wichi <barbozasebastian408@gmail.com>
2026-01-18 18:32:36 -0400

    Actualizar tools-bratvideo.js

--- plugins/tools-bratvideo.js
+++ plugins/tools-bratvideo.js
@@ -3,27 +3,26 @@ import { sticker } from '../lib/sticker.js'
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
     try {
-        if (!text) return conn.reply(m.chat, `❀ *Uso Correcto:*\n${usedPrefix + command} texto | color | fondo | tipo\n\n*Ejemplo:*\n${usedPrefix + command} Barbosa | Blanco | Negro | José`, m)
+        if (!text) return conn.reply(m.chat, `❀ *Formato en inglés:*\n${usedPrefix + command} texto | color | fondo | tipo\n\n*Ejemplo:*\n${usedPrefix + command} Hello | white | black | Jose`, m)
         
         await m.react('🕒')
 
         let [txt, color, fondo, tipo] = text.split('|').map(v => v.trim())
 
         let textoFinal = txt
-        let letraFinal = color || 'Blanco'
-        let fondoFinal = fondo || 'Negro'
-        let tipoFinal = tipo || 'José'
+        let letraFinal = color || 'white'
+        let fondoFinal = fondo || 'black'
+        let tipoFinal = tipo || 'Jose'
         let apiKey = 'sylphy-6f150d'
 
         let apiUrl = `https://sylphy.xyz/tools/brat?text=${encodeURIComponent(textoFinal)}&color=${encodeURIComponent(letraFinal)}&fondo=${encodeURIComponent(fondoFinal)}&type=${encodeURIComponent(tipoFinal)}&api_key=${apiKey}`
 
         const res = await fetch(apiUrl)
         
-        if (!res.ok) throw 'Error al conectar con Sylphy API'
+        if (!res.ok) throw 'Error'
         
         const buffer = await res.buffer()
-        
-        const stiker = await sticker(buffer, false, global.packname || 'Brat', global.author || 'Bot')
+        const stiker = await sticker(buffer, false, 'Brat Bot', 'S-Bot')
 
         if (stiker) {
             await conn.sendFile(m.chat, stiker, 'brat.webp', '', m)
@@ -32,9 +31,9 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 
     } catch (e) {
         await m.react('✖️')
-        conn.reply(m.chat, `⚠︎ *Error al generar el sticker*\n\nIntenta con:\n${usedPrefix + command} ${text || 'Barbosa'} | Blanco | Negro | José`, m)
+        conn.reply(m.chat, `⚠︎ Error. Ejemplo: ${usedPrefix + command} Hello | white | black | Jose`, m)
     }
 }
 
-handler.command = /^(brat|brat4)$/i
+handler.command = /^(brat|sbrat)$/i
 export default handler
