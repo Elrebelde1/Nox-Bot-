7adaa0492f076efc35cf58fdcf002c8bae932182
Wichi <barbozasebastian408@gmail.com>
2026-01-19 14:17:28 -0400

    Actualizar descargas-tiktokuser.js

--- plugins/descargas-tiktokuser.js
+++ plugins/descargas-tiktokuser.js
@@ -57,6 +57,6 @@ let handler = async (m, { conn, usedPrefix, command, text}) => {
 handler.tags = ['downloader'];
 handler.help = ['tiktokuser *<usuario>*'];
 handler.command = ['tiktokuser', 'tiktokus'];
-handler.register = false;
+handler.register = true;
 
 export default handler;
