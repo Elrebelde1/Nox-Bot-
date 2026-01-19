5775ba38b28fbe745aadef549eec734956cb0186
Wichi <barbozasebastian408@gmail.com>
2026-01-19 14:17:16 -0400

    Actualizar descargas-xvideosearch.js

--- plugins/descargas-xvideosearch.js
+++ plugins/descargas-xvideosearch.js
@@ -82,6 +82,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['xvideosearch'];
 handler.command = ['xvideosearch', 'xvideosearch'];
-handler.register = false
+handler.register = true
 
 export default handler;
