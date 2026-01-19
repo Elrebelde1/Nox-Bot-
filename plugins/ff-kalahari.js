7c02f57d209ec1d1b81850b19f30e73a51278b39
Wichi <barbozasebastian408@gmail.com>
2026-01-19 14:16:54 -0400

    Actualizar ff-kalahari.js

--- plugins/ff-kalahari.js
+++ plugins/ff-kalahari.js
@@ -23,6 +23,6 @@ await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
 global.db.data.users[m.sender].lastcofre = new Date * 1
 }
 handler.command = ['kalahari'] 
-handler.register = false
+handler.register = true
 handler.admin = true
 export default handler
