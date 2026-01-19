301b39b5461f031458ffc1321201abcd260fd526
Wichi <barbozasebastian408@gmail.com>
2026-01-19 14:22:10 -0400

    Actualizar ff-nextera.js

--- plugins/ff-nextera.js
+++ plugins/ff-nextera.js
@@ -27,6 +27,6 @@ let handler = async (m, { isPrems, conn }) => {
 };
 
 handler.command = ['nexterra']; 
-handler.register = false
+handler.register = true
 handler.admin = true;
 export default handler;
