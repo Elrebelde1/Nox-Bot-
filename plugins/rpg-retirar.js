5b126848b749bb369c23d2b6cf6b6c8175e39799
Wichi <barbozasebastian408@gmail.com>
2026-01-19 14:16:16 -0400

    Actualizar rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -22,5 +22,5 @@ let handler = async (m, { args }) => {
 handler.help = ['retirar']
 handler.tags = ['rpg']
 handler.command = ['withdraw', 'retirar', 'wd']
-handler.register = false
+handler.register = true
 export default handler
