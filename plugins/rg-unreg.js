2f7a2d185f3d7456886ef455c6073038d9d08a38
Wichi <barbozasebastian408@gmail.com>
2026-01-19 14:13:24 -0400

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -10,5 +10,5 @@ return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m
 handler.help = ['unreg']
 handler.tags = ['rg']
 handler.command = /^unreg(ister)?$/i
-handler.register = false
+handler.register = true
 export default handler
