a40edf0160cb265b108dbb77c25aab2478f6e200
Wichi <barbozasebastian408@gmail.com>
2026-01-19 14:13:56 -0400

    Actualizar group-poll.js

--- plugins/group-poll.js
+++ plugins/group-poll.js
@@ -11,6 +11,6 @@ handler.tags = ['group']
 handler.help = ['encuesta *<pregunta|opciones>*']
 handler.command = ['encuesta', 'poll']
 handler.group = true
-handler.register = false
+handler.register = true
 
 export default handler
