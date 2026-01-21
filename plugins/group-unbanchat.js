b39f216c2c624f0d8a2008853474c47ecc9c25ea
Wichi <barbozasebastian408@gmail.com>
2026-01-20 20:54:12 -0400

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,11 +1,23 @@
-let handler = async (m, { conn, isAdmin, isROwner} ) => {
+let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+    
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '✅ Bot activo en este grupo.', m, rcanal)
-    await m.react('✅')
+    
+    let txt = `┏━━━━━━━━━━━━━━━━━━┓\n`
+    txt += `┃ ✨ *BOT ACTIVADO* ✨\n`
+    txt += `┃━━━━━━━━━━━━━━━━━━┃\n`
+    txt += `┃ 📝 *Estado:* Operativo\n`
+    txt += `┃ 🛡️ *Admin:* ${m.pushName}\n`
+    txt += `┃ ✅ *Listo para usar*\n`
+    txt += `┗━━━━━━━━━━━━━━━━━━┛`
+
+    await conn.reply(m.chat, txt, m, rcanal)
+    await m.react('🔋')
 }
+
 handler.help = ['desbanearbot']
 handler.tags = ['group']
 handler.command = ['desbanearbot', 'unbanchat']
 handler.group = true 
+
 export default handler
