1c8f1b08b035c30ffd64078e1cc77a71d86e2e95
Wichi <barbozasebastian408@gmail.com>
2026-02-06 22:10:33 -0400

    Actualizar group-fantasmas.js

--- plugins/group-fantasmas.js
+++ plugins/group-fantasmas.js
@@ -23,5 +23,5 @@ handler.tags = ['group']
 handler.command = /^(verfantasmas|fantasmas|sider)$/i
 handler.admin = true
 handler.botAdmin = true
-handler.register = true
+handler.register = true 
 export default handler
