7b3f5cdd4b21271745fdf228ee443455f34986c4
Wichi <barbozasebastian408@gmail.com>
2026-04-17 22:33:33 -0400

    Actualizar btns_canales.js

--- plugins/btns_canales.js
+++ plugins/btns_canales.js
@@ -2,19 +2,18 @@ let handler = async (m, { conn, text }) => {
   const canal1 = "https://whatsapp.com/channel/0029Vb8kvXUBfxnzYWsbS81I"
   const canal2 = "https://whatsapp.com/channel/0029VbBbaFCAO7RL7UEhBD2F"
 
-  // Detecta el ID del botón presionado
   let id = text || m.text
 
-  if (id === 'sask_c1') {
-    let txt = `✨ *¡Hola! No olvides seguir nuestro canal oficial para estar al tanto de cada actualización.* 👤⚡\n\n🔗 *Canal Principal:* ${canal1}\n\n> 𝖡𝗒 𝖡𝖺𝗋𝖻𝗈𝗓𝖺-𝖳𝖾𝖺𝗆 ⚡`
-    await conn.reply(m.chat, txt, m)
-  }
-
-  if (id === 'sask_c2') {
-    let txt = `🚀 *¿Necesitas ayuda o quieres conocer a la comunidad? Únete a nuestro canal de soporte.* ⚡\n\n🔗 *Canal de Soporte:* ${canal2}\n\n> 𝖡𝗒 𝖡𝖺𝗋𝖻𝗈𝗓𝖺-𝖳𝖾𝖺𝗆 ⚡`
+  if (id === 'ver_canales') {
+    let txt = `✨ *¡Hola! Forma parte de nuestra comunidad oficial.* 👤⚡\n\n`
+    txt += `Sigue nuestros canales para no perderte ninguna actualización de **Sasuke Bot MD**.\n\n`
+    txt += `🔗 *Canal Principal:* ${canal1}\n\n`
+    txt += `🚀 *Canal de Soporte:* ${canal2}\n\n`
+    txt += `> 𝖡𝗒 𝖡𝖺𝗋𝖻𝗈𝗓𝖺-𝖳𝖾𝖺ᴍ ⚡`
+    
     await conn.reply(m.chat, txt, m)
   }
 }
 
-handler.command = ['sask_c1', 'sask_c2']
+handler.command = ['ver_canales']
 export default handler
