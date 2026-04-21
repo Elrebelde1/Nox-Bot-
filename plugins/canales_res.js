b9a0f0f6fb8c3461cf7b337faa81e1a15a28ea62
Wichi <barbozasebastian408@gmail.com>
2026-04-20 21:58:46 -0400

    Actualizar canales_res.js

--- plugins/canales_res.js
+++ plugins/canales_res.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, command }) => {
   const canal1 = "https://whatsapp.com/channel/0029Vb8kvXUBfxnzYWsbS81I"
   const canal2 = "https://whatsapp.com/channel/0029VbBbaFCAO7RL7UEhBD2F"
+  const canal3 = "https://whatsapp.com/channel/0029VbCdg9rJf05duUBwlh2o"
 
   if (command === 'canal1') {
     let txt = `✨ *¡Hola! No olvides seguir nuestro canal principal para todas las novedades de Sasuke Bot.* 👤⚡\n\n🔗 *Únete aquí:* ${canal1}`
@@ -11,7 +12,12 @@ let handler = async (m, { conn, command }) => {
     let txt = `🚀 *¡Ey! Te invitamos a formar parte de nuestra comunidad oficial de apoyo.* ⚡\n\n🔗 *Únete aquí:* ${canal2}`
     await conn.reply(m.chat, txt, m)
   }
+
+  if (command === 'canal3') {
+    let txt = `🤡 *¿Buscas reírte un rato? Únete a nuestro canal de memes oficial.* ⚡\n\n🔗 *Únete aquí:* ${canal3}`
+    await conn.reply(m.chat, txt, m)
+  }
 }
 
-handler.command = ['canal1', 'canal2']
+handler.command = ['canal1', 'canal2', 'canal3']
 export default handler
