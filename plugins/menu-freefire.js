af43120d237e4270653cdb2f0c59125ae4851482
Wichi <barbozasebastian408@gmail.com>
2026-04-26 00:10:51 -0400

    Actualizar menu-freefire.js

--- plugins/menu-freefire.js
+++ plugins/menu-freefire.js
@@ -1,55 +1,55 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix }) => {
+  let doc = {
+    text: `╔══🔥 • 𝕾𝕬𝕾𝖀𝕶𝕰 𝕭𝕺𝕿 • 🔥══╗
+   👑  𝐌𝐄𝐍Ú 𝐅𝐑𝐄𝐄 𝐅𝐈𝐑𝐄  👑
+╚════════════════════╝
 
-  let texto = `『 ⚔️ 𝐒𝐀𝐒𝐔𝐊𝐄 𝐁𝐎𝐓 𝐌𝐃 - 𝐎𝐏𝐄𝐑𝐀𝐓𝐈𝐕𝐎 ⚔️ 』
+📍 *USUARIO:* @${m.sender.split('@')[0]}
 
-「 👤 𝐔𝐒𝐔𝐀𝐑𝐈𝐎: @${m.sender.split('@')[0]} 」
-「 📂 𝐒𝐓𝐀𝐓𝐔𝐒: Acceso Autorizado 」
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ 🎮 *CLAN GESTIÓN*
+┗━━━━━━━━━━━━━━━━━━━━┛
+🖤 ➺ ${usedPrefix}donarsala
+🖤 ➺ ${usedPrefix}reglas
+🖤 ➺ ${usedPrefix}setreglas
+🖤 ➺ ${usedPrefix}borrareglas
 
-◈───── ⚡ 𝐅𝐑𝐄𝐄 𝐅𝐈𝐑𝐄 ⚡ ─────◈
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ 🗺️ *MAPAS ESTRATÉGICOS*
+┗━━━━━━━━━━━━━━━━━━━━┛
+🖤 ➺ ${usedPrefix}mapa bermuda
+🖤 ➺ ${usedPrefix}mapa purgatorio
+🖤 ➺ ${usedPrefix}mapa kalahari
+🖤 ➺ ${usedPrefix}mapa nexterra
+🖤 ➺ ${usedPrefix}mapa alpes
 
- 🛡️  *GESTIÓN DE CLAN*
- ╰ 🎫 ${usedPrefix}donarsala
- ╰ 📜 ${usedPrefix}reglas
- ╰ ⚙️ ${usedPrefix}setreglas
- ╰ 🗑️ ${usedPrefix}borrareglas
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ ⚔️ *MODOS VERSUS*
+┗━━━━━━━━━━━━━━━━━━━━┛
+⚔ ➺ .4vs4 | .6vs6 | .8vs8
+⚔ ➺ .12vs12 | .scrim
 
- 🗺️  *RECONOCIMIENTO DE MAPAS*
- ╰ 📍 ${usedPrefix}mapa bermuda
- ╰ 📍 ${usedPrefix}mapa purgatorio
- ╰ 📍 ${usedPrefix}mapa kalahari
- ╰ 📍 ${usedPrefix}mapa nexterra
- ╰ 📍 ${usedPrefix}mapa alpes
-
- ⚔️  *MODOS DE COMBATE*
- ╰ 🥊 4vs4 | 6vs6 | 8vs8
- ╰ 💀 12vs12 | 16vs16
- ╰ 🚩 20vs20 | 24vs24
- ╰ 🏆 .scrim
-
-◈─────────────────────────◈
-📡 [ 𝙲𝙾𝙽𝙴𝚇𝙸𝙾́𝙽 𝙴𝚂𝚃𝙰𝙱𝙻𝙴 ]
-✨ 𝑺𝒂𝒔𝒖𝒌𝒆 𝑩𝒐𝒕 | 𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒂𝒓𝒃𝒐𝒛𝒂 ✨`
-
-  // Mensaje con mención al usuario que lo solicita
-  await conn.sendMessage(m.chat, { 
-    text: texto,
-    mentions: [m.sender],
+*◈────────── • ☄️ • ──────────◈*
+✨ 𝑺𝒂𝒔𝒖𝒌𝒆 𝑩𝒐𝒕 | 𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒂𝒓𝒃𝒐𝒛𝒂 ✨`,
     contextInfo: {
+      mentionedJid: [m.sender],
       externalAdReply: {
-        title: "Sᴀsᴜᴋᴇ Bᴏᴛ - Cᴏɴᴛʀᴏʟ Gᴀᴍᴇʀ",
-        body: "Bᴀʀʙᴏᴢᴀ Dᴇᴠᴇʟᴏᴘᴇʀ",
+        title: "SASUKE BOT - FREE FIRE",
+        body: "Menu de Comandos",
         thumbnailUrl: "https://files.catbox.moe/qmmttw.jpg",
         sourceUrl: "https://whatsapp.com/channel/0029Vb8kvXUBfxnzYWsbS81I",
         mediaType: 1,
-        renderLargerThumbnail: false
+        showAdAttribution: true,
+        renderLargerThumbnail: true
       }
     }
-  }, { quoted: m })
+  }
+
+  await conn.sendMessage(m.chat, doc, { quoted: m })
 }
 
 handler.help = ['ff']
-handler.tags = ['freefire']
-handler.command = /^(ff|menuff|fuego|garena)$/i
-handler.register = true
+handler.tags = ['main']
+handler.command = ['ff', 'menuff', 'freefire']
 
 export default handler
