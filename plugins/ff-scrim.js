5e164dbd5c68ef96c53776bc94e82b2fe2ab5763
Wichi <barbozasebastian408@gmail.com>
2026-04-26 16:13:05 -0400

    Actualizar ff-scrim.js

--- plugins/ff-scrim.js
+++ plugins/ff-scrim.js
@@ -1,33 +1,47 @@
-import fg from 'api-dylux' 
-import fetch from 'node-fetch'
-import axios from 'axios'
-let handler = async (m, { conn, args, command, usedPrefix }) => {
-if (!args[0]) throw `
-• 𝐒𝐂𝐑𝐈𝐌 •
+let handler = async (m, { conn, usedPrefix, command }) => {
 
-⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎                       •
-🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎 : 
-🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀 :                
+let scrim = `╔══🔥 • 𝕾𝕬𝕾𝖀𝕶𝕰 𝕭𝕺𝕿 • 🔥══╗
+   ⚔️ 🔱  𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐎 𝐒𝐂𝐑𝐈𝐌  🔱 ⚔️
+╚════════════════════╝
 
-➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃: 
-➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ ✨  *ESCUADRA DE ÉLITE* ✨
+┗━━━━━━━━━━━━━━━━━━━━┛
+  🏮 1. • 👑 
+  🏮 2. • ⚡ 
+  🏮 3. • ⚡ 
+  🏮 4. • ⚡ 
 
-      𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 
-    
-    👑 ┇ 
-    🥷🏻 ┇  
-    🥷🏻 ┇ 
-    🥷🏻 ┇  
-    
-    ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:
-    🥷🏻 ┇ 
-    🥷🏻 ┇
-                 
-` 
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ 🛡️  *RESERVAS MÉDICAS* 🛡️
+┗━━━━━━━━━━━━━━━━━━━━┛
+  🧪 5. • 🧱 
+  🧪 6. • 🧱 
+
+*⊱───────────────────⊰*
+   🔥 𝑼𝒏𝒆𝒕𝒆 𝒂𝒍 𝑰𝒏𝒇ِي𝒆𝒓𝒏𝒐 𝑪𝒍𝒂𝒏 🔥
+*⊱───────────────────⊰*
+
+👉 *COPIA Y ANÓTATE*`
+
+await conn.sendMessage(m.chat, { 
+    text: scrim,
+    contextInfo: {
+        externalAdReply: {
+            title: "SASUKE BOT - SCRIM FF",
+            body: "Barboza Developer",
+            thumbnailUrl: "https://files.catbox.moe/qmmttw.jpg",
+            sourceUrl: "https://whatsapp.com/channel/0029Vb8kvXUBfxnzYWsbS81I",
+            mediaType: 1,
+            renderLargerThumbnail: true
+        }
+    }
+}, { quoted: m })
+
 }
 handler.help = ['scrim']
 handler.tags = ['freefire']
 handler.command = /^(scrim)$/i
 handler.group = true
-handler.admin = false
+
 export default handler
