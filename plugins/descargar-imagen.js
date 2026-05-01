50edaedc8f5e63eba88a921bfa5ac28395dc1773
Wichi <barbozasebastian408@gmail.com>
2026-05-01 11:42:40 -0400

    Actualizar descargar-imagen.js

--- plugins/descargar-imagen.js
+++ plugins/descargar-imagen.js
@@ -1,42 +1,28 @@
-import axios from 'axios'
+import fetch from "node-fetch"
 
-const handler = async (m, { conn, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `❀ Por favor, ingrese un texto para buscar una Imagen.`, m)
-try {
-await m.react('🕒')
-const res = await getGoogleImageSearch(text)
-const urls = await res.getAll()
-if (urls.length < 2) return conn.reply(m.chat, '✧ No se encontraron suficientes imágenes para un álbum.', m)
-const medias = urls.slice(0, 10).map(url => ({ type: 'image', data: { url } }))
-const caption = `❀ Resultados de búsqueda para: ${text}`
-await conn.sendSylphy(m.chat, medias, { caption, quoted: m })
-await m.react('✔️')
-} catch (error) {
-await m.react('✖️')
-conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${error.message}`, m)
-}}
+const handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) return m.reply(`*Ingrese su consulta*\n\n*Ejemplo:* ${usedPrefix}${command} ¿Quién es Messi?`)
 
-handler.help = ['imagen']
-handler.tags = ['descargas']
-handler.command = ['imagen', 'image']
+    await m.react('💬')
+
+    try {
+        const res = await fetch(`https://api.delirius.store/ia/chatgpt?q=${encodeURIComponent(text)}`)
+        const json = await res.json()
+
+        if (!json.status || !json.data) {
+            await m.react('❌')
+            return m.reply('⚠️ Error API')
+        }
+
+        await m.reply(json.data)
+        await m.react('✅')
+
+    } catch (e) {
+        await m.react('❌')
+        m.reply('🛑 Error')
+    }
+}
+
+handler.command = ['chatgpt2', 'ia2']
 
 export default handler
-
-function getGoogleImageSearch(query) {
-const apis = [`${global.APIs.delirius.url}/search/gimage?query=${encodeURIComponent(query)}`, `${global.APIs.siputzx.url}/api/images?query=${encodeURIComponent(query)}`]
-return { getAll: async () => {
-for (const url of apis) {
-try {
-const res = await axios.get(url)
-const data = res.data
-if (Array.isArray(data?.data)) {
-const urls = data.data.map(d => d.url).filter(u => typeof u === 'string' && u.startsWith('http'))
-if (urls.length) return urls
-}} catch {}
-}
-return []
-},
-getRandom: async () => {
-const all = await this.getAll()
-return all[Math.floor(Math.random() * all.length)] || null
-}}}
