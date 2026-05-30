68bcb730402c427bd96922a5d142799e0b837a69
Wichi <barbozasebastian408@gmail.com>
2026-05-29 21:37:21 -0400

    Actualizar _antifakes.js

--- plugins/_antifakes.js
+++ plugins/_antifakes.js
@@ -1,52 +1,12 @@
-import db from '../lib/database.js'
-let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
-if (!m.isGroup) return !1
-let chat = global.db.data.chats[m.chat]
-if (isBotAdmin && chat.antifake) {
-if (m.sender.startsWith('6' || '6')) {
-global.db.data.users[m.sender].block = true
+let handler = async (m, { conn }) => {
+    // Verifica si el comando se está usando en un grupo
+    if (!m.isGroup) return m.reply('⚠️ Este comando solo puede ser utilizado dentro de un grupo.')
 
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('90' || '90')) {
-global.db.data.users[m.sender].block = true
+    // Envía el ID del grupo actual
+    await conn.reply(m.chat, `🆔 *ID de este grupo:* \n\n\`${m.chat}\``, m)
+}
 
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('212' || '212')) {
-global.db.data.users[m.sender].block = true
+handler.help = ['id']
+handler.command = /^(id|jid|groupid)$/i
 
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('92' || '92')) {
-global.db.data.users[m.sender].block = true
-
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('93' || '93')) {
-global.db.data.users[m.sender].block = true
-
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('94' || '94')) {
-global.db.data.users[m.sender].block = true
-
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('7' || '7')) {
-global.db.data.users[m.sender].block = true
-
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('49' || '49')) {
-global.db.data.users[m.sender].block = true
-
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('2' || '2')) {
-global.db.data.users[m.sender].block = true
-
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('91' || '91')) {
-global.db.data.users[m.sender].block = true
-
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('63' || '63')) {
-global.db.data.users[m.sender].block = true
-
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-}}
 export default handler
