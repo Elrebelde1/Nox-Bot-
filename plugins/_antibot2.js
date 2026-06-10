c58025eeff27013e7e96d1c8191652506a90302f
Wichi <barbozasebastian408@gmail.com>
2026-06-09 23:34:12 -0400

    Actualizar _antibot2.js

--- plugins/_antibot2.js
+++ plugins/_antibot2.js
@@ -1,24 +1,29 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!m.quoted) return m.reply(`🏮 *𝘚𝘢𝘴𝘶𝘬𝘦 𝘜𝘤𝘩𝘪𝘩𝘢 𝘉𝘰𝘵* 🏮\n⚠️ _Responde al sticker que deseas asignar como comando con:_ *${usedPrefix + command} <comando/texto>*`)
-  if (!text) return m.reply(`🏮 *𝘚𝘢𝘴𝘶𝘬𝘦 𝘜𝘤𝘩𝘪𝘩𝘢 𝘉𝘰𝘵* 🏮\n⚠️ _Debes ingresar el comando o texto a ejecutar._\nEjemplo: *${usedPrefix + command} #menu*`)
+  if (!m.quoted) return m.reply(`🏮 *𝘚𝘢𝘴𝘶𝘬𝘦 𝘜𝘤𝘩𝘪𝘩𝘢 𝘉𝘰𝘵* 🏮\n⚠️ _Responde al sticker que deseas asignar como comando con:_ *${usedPrefix + command} <archivo.js>*`)
+  if (!text) return m.reply(`🏮 *𝘚𝘢𝘴𝘶𝘬𝘦 𝘜𝘤𝘩𝘪𝘩𝘢 𝘉𝘰𝘵* 🏮\n⚠️ _Debes ingresar el nombre del archivo._\nEjemplo: *${usedPrefix + command} menu.js*`)
   
   let q = m.quoted
   let mime = (q.msg || q).mimetype || ''
   if (!/webp/.test(mime)) return m.reply('👁️‍🗨️ _El mensaje respondido debe ser estrictamente un sticker._')
   if (!q.fileSha256) return m.reply('⚡ _No se pudo generar el hash identificador del sticker._')
 
+  let comandoFinal = text.trim()
+  if (!comandoFinal.endsWith('.js')) {
+    comandoFinal += '.js'
+  }
+
   let hash = Buffer.from(q.fileSha256).toString('base64')
   if (!global.db.data.sticker) global.db.data.sticker = {}
   
   global.db.data.sticker[hash] = {
-    text: text,
+    text: comandoFinal,
     mentionedJid: m.mentionedJid || []
   }
 
-  m.reply(`⛩️ *𝘑𝘶𝘵𝘴𝘶: 𝘊𝘰𝘮𝘢𝘯𝘥𝘰 𝘈𝘴𝘪𝘨𝘯𝘢𝘥𝘰* ⛩️\n\n✅ *𝘚𝘵𝘪𝘤𝘬𝘦𝘳:* \`${hash.substring(0, 10)}...\`\n💬 *𝘊𝘰𝘮𝘢𝘯𝘥𝘰:* \`${text}\``)
+  m.reply(`⛩️ *𝘑𝘶𝘵𝘴𝘶: 𝘊𝘰𝘮𝘢𝘯𝘥𝘰 𝘈𝘴𝘪𝘨𝘯𝘢𝘥𝘰* ⛩️\n\n✅ *𝘚𝘵𝘪𝘤𝘬𝘦𝘳:* \`${hash.substring(0, 10)}...\`\n💬 *𝘊𝘰𝘮𝘢𝘯𝘥𝘰:* \`${comandoFinal}\``)
 }
 
-handler.help = ['setcmd <texto>']
+handler.help = ['setcmd <archivo.js>']
 handler.tags = ['database']
 handler.command = ['setcmd', 'addcmd']
 handler.rowner = true
