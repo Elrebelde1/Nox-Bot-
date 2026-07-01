e8b5332e456e6e67a3c78f970f118a3704652993
Wichi <barbozasebastian408@gmail.com>
2026-07-01 02:38:14 -0400

    Actualizar group-top.js

--- plugins/group-top.js
+++ plugins/group-top.js
@@ -1,28 +1,40 @@
-let handler = async (m, { conn, participants, text }) => {
-    if (!text) return m.reply('Indica el top, idiota.')
+import util from 'util'
+import path from 'path'
+let user = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
+if (!text) return m.reply(`Ejemplo de uso:\n.top *texto*`)
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b = ps.getRandom()
+let c = ps.getRandom()
+let d = ps.getRandom()
+let e = ps.getRandom()
+let f = ps.getRandom()
+let g = ps.getRandom()
+let h = ps.getRandom()
+let i = ps.getRandom()
+let j = ps.getRandom()
+let k = Math.floor(Math.random() * 70);
+let x = `${pickRandom(['🤓','😅','😂','😳','😎', '🥵', '😱', '🤑', '🙄', '💩','🍑','🤨','🥴','🔥','👇🏻','😔', '👀','🌚','🗿'])}`
+let l = Math.floor(Math.random() * x.length);
+let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
+let top = `*${x} Top 10 ${text} ${x}*
     
-    let members = participants.map(u => u.id)
-    let selected = []
-    
-    while (selected.length < 10 && selected.length < members.length) {
-        let user = members[Math.floor(Math.random() * members.length)]
-        if (!selected.includes(user)) selected.push(user)
-    }
-    
-    let list = selected.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
-    
-    let result = `*Top 10 ${text}*
-
-${list}
-
-Desarrollado por: Sebastián Barboza`
-    
-    conn.reply(m.chat, result, m, { mentions: selected })
+*1. ${user(a)}*
+*2. ${user(b)}*
+*3. ${user(c)}*
+*4. ${user(d)}*
+*5. ${user(e)}*
+*6. ${user(f)}*
+*7. ${user(g)}*
+*8. ${user(h)}*
+*9. ${user(i)}*
+*10. ${user(j)}*`
+m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
 }
-
-handler.help = ['top']
-handler.tags = ['grupos']
-handler.command = ['top']
+handler.help = handler.command = ['top']
+handler.tags = ['fun']
 handler.group = true
-
 export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
