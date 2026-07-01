6c23824bdf0951a96227f5d7ed2aef09fcb5203f
Wichi <barbozasebastian408@gmail.com>
2026-07-01 01:34:02 -0400

    Crear main-info.js

---
+++ plugins/main-info.js
@@ -0,0 +1,7 @@
+let handler = async (m) => {
+    m.reply(`Información del sistema cargada.`)
+}
+handler.help = ['info']
+handler.tags = ['main']
+handler.command = ['info']
+export default handler
