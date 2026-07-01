119ecffdbb5e5c6cf99e1d9d4b8db92826dfc939
Wichi <barbozasebastian408@gmail.com>
2026-06-30 22:56:41 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -2,137 +2,113 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
-import * as cheerio from 'cheerio';
+import * as cheerio from 'cheerio'
 import fetch from 'node-fetch'
 import axios from 'axios'
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-  global.owner = [
-  [ '172056423456949@lid', 'Barboza OFC', true ],
-  [ '584226480049', 'Goku', true ],
-  [ '5716540912', 'nopclip', true ],
-  [ '595972157130'],
-  [ '14388165130', 'Rayo OFC', true ],
-  [ '573136602975', "I'm Fz ~", true ],
-  [ '573136602975', 'juanito', true ],
-  [ '51928626068', "MediaHub",true ],
-  [ '172056423456949', true ]
-  ]
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.owner = [
+  [ '584165341214', 'Barboza OFC 🌃', true ]
+]
 
 global.mods = []
 global.prems = []
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.packname = `[ 𝙎𝙖𝙨𝙪𝙠𝙚 𝘽𝙤𝙩 🌀`
-global.author = '𝙎𝙖𝙨𝙪𝙠𝙚 𝘽𝙤𝙩 𝙈𝘿 🌀]'
-global.stickpack = '© 𝙎𝙖𝙨𝙪𝙠𝙚 𝘽𝙤𝙩 𝙈𝘿 🌀'
-global.stickauth = '𝘽𝙮 𝙎𝙖𝙨𝙪𝙠𝙚 𝘽𝙤𝙩'
-global.wm = '𝙎𝙖𝙨𝙪𝙠𝙚 🌀'
-global.dev = '© 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝙎𝙪𝙣𝙛𝙡𝙖𝙧𝙚  ☂︎  𝙏𝙚𝙖𝙢'
-global.wait = '🌪️ *𝘼𝙜𝙪𝙖𝙧𝙙𝙚 𝙪𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤, 𝙨𝙤𝙮 𝙡𝙚𝙣𝙩𝙤... ฅ^•ﻌ•^ฅ\n\n> 𝙎𝙖𝙨𝙪𝙠𝙚 𝘽𝙤𝙩 𝙈𝘿 🌀 🌪️*'
-global.botname = '[ 𝙎𝙖𝙨𝙪𝙠𝙚 𝘽𝙤𝙩 𝙈𝘿 🌀 ]'
-global.textbot = `𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝙎𝙖𝙨𝙪𝙠𝙚 🌀`
-global.dev = '• 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝘾𝙤𝙢𝙢𝙪𝙣𝙞𝙩𝙮 𝙎𝙖𝙨𝙪𝙠𝙚 𝘽𝙤𝙩 𝙈𝘿 🌀'
+global.packname = `[ 𝙉𝙤𝙭 𝘽ο𝙩 🌃`
+global.author = '𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃]'
+global.stickpack = '© 𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃'
+global.stickauth = '𝘽𝙮 𝙉𝙤𝙭 𝘽ο𝙩'
+global.wm = '𝙉𝙤𝙭 🌃'
+global.botname = '[ 𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃 ]'
+global.textbot = `𝙋ο𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝙉𝙤𝙭 🌀`
+global.dev = '• 𝙋ο𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝘾ο𝙢𝙢𝙪𝙣𝙞𝙩𝙮 𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃'
+global.wait = '🌪️ *𝘼𝙜𝙪𝙖𝙧𝙙𝙚 𝙪𝙣 𝙢ο𝙢𝙚𝙣𝙩ο, 𝙨ο𝙮 𝙡𝙚𝙣𝙩ο... ฅ^•ﻌ•^ฅ\n\n> 𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃 🌪️*'
 global.listo = '*𝘼𝙦𝙪𝙞 𝙩𝙞𝙚𝙣𝙚 ฅ^•ﻌ•^ฅ*'
-global.namechannel = '𝙎𝙖𝙨𝙪𝙠𝙚 𝘽𝙤𝙩 𝙈𝘿 🌀'
+global.namechannel = '𝙉𝙤𝙭 𝘽ο𝙩 𝙈𝘿 🌃'
 global.channel = 'https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18'
-
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
 global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 global.group = 'https://chat.whatsapp.com/CBuLXuVZcg9FEfCSHiY6b0'
 global.canal = 'https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18'
-global.insta = 'https://www.insta.com/https://www.insta.com/sebastian_barboza13'
+global.insta = 'https://www.insta.com/sebastian_barboza13'
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.catalogo = fs.readFileSync('./storage/img/catalogo.png');
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
-
-global.ch = {
-ch1: '120363414007802886@newsletter',
-ch2: '120363423619689248@newsletter',
-ch3: '120363409416185213@newsletter',
-ch4: '120363426372470669@newsletter'
-
+global.estilo = { 
+  key: { 
+    fromMe: false, 
+    participant: `0@s.whatsapp.net`, 
+    ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) 
+  }, 
+  message: { 
+    orderMessage: { 
+      itemCount: -999999, 
+      status: 1, 
+      surface: 1, 
+      message: global.packname, 
+      orderTitle: 'Bang', 
+      thumbnail: global.catalogo, 
+      sellerJid: '0@s.whatsapp.net'
+    }
+  }
 }
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
-global.jadi = 'Sesiones/Subbots'
-global.Sesion = 'Sesiones/Principal'
+global.Sesion = 'Principal'
 global.dbname = 'database.json'
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 global.multiplier = 69 
-global.maxwarn = '2' // máxima advertencias
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-//Apis
+global.maxwarn = '2' 
 
 global.APIs = {
-amel: 'https://melcanz.com',
-bx: 'https://bx-hunter.herokuapp.com',
-nrtm: 'https://nurutomo.herokuapp.com',
-xteam: 'https://api.xteam.xyz',
-nzcha: 'http://nzcha-apii.herokuapp.com',
-bg: 'http://bochil.ddns.net',
-fdci: 'https://api.fdci.se',
-dzx: 'https://api.dhamzxploit.my.id',
-bsbt: 'https://bsbt-api-rest.herokuapp.com',
-zahir: 'https://zahirr-web.herokuapp.com',
-zeks: 'https://api.zeks.me',
-hardianto: 'https://hardianto-chan.herokuapp.com',
-pencarikode: 'https://pencarikode.xyz',
-LeysCoder: 'https://leyscoders-api.herokuapp.com',
-adiisus: 'https://adiixyzapi.herokuapp.com',
-lol: 'https://api.lolhuman.xyz',
-fgmods: 'https://api-fgmods.ddns.net',
-pencarikode: 'https://pencarikode.xyz',
-Velgrynd: 'https://velgrynd.herokuapp.com',
-rey: 'https://server-api-rey.herokuapp.com',
-hardianto: 'http://hardianto-chan.herokuapp.com',
-shadow: 'https://api.reysekha.xyz',
-apialc: 'https://api-alc.herokuapp.com',
-botstyle: 'https://botstyle-api.herokuapp.com',
-neoxr: 'https://neoxr-api.herokuapp.com',
-ana: 'https://anabotofc.herokuapp.com/',
-kanx: 'https://kannxapi.herokuapp.com/',
-dhnjing: 'https://dhnjing.xyz'
-},
+  amel: 'https://melcanz.com',
+  bx: 'https://bx-hunter.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  adiisus: 'https://adiixyzapi.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz',
+  fgmods: 'https://api-fgmods.ddns.net',
+  Velgrynd: 'https://velgrynd.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
+  shadow: 'https://api.reysekha.xyz',
+  apialc: 'https://api-alc.herokuapp.com',
+  botstyle: 'https://botstyle-api.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  ana: 'https://anabotofc.herokuapp.com/',
+  kanx: 'https://kannxapi.herokuapp.com/',
+  dhnjing: 'https://dhnjing.xyz'
+}
 
 global.APIKeys = {
-'https://api-alc.herokuapp.com': 'ConfuMods',
-'https://api.reysekha.xyz': 'apirey',
-'https://melcanz.com': 'F3bOrWzY',
-'https://bx-hunter.herokuapp.com': 'Ikyy69',
-'https://api.xteam.xyz': '5bd33b276d41d6b4',
-'https://zahirr-web.herokuapp.com': 'zahirgans',
-'https://bsbt-api-rest.herokuapp.com': 'benniismael',
-'https://api.zeks.me': 'apivinz',
-'https://hardianto-chan.herokuapp.com': 'hardianto',
-'https://pencarikode.xyz': 'pais',
-'https://api-fgmods.ddns.net': 'fg-dylux',
-'https://leyscoders-api.herokuapp.com': 'MIMINGANZ',
-'https://server-api-rey.herokuapp.com': 'apirey',
-'https://api.lolhuman.xyz': 'GataDiosV2',
-'https://botstyle-api.herokuapp.com': 'Eyar749L',
-'https://neoxr-api.herokuapp.com': 'yntkts',
-'https://anabotofc.herokuapp.com/': 'AnaBot'
+  'https://api-alc.herokuapp.com': 'ConfuMods',
+  'https://api.reysekha.xyz': 'apirey',
+  'https://melcanz.com': 'F3bOrWzY',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+  'https://api.xteam.xyz': '5bd33b276d41d6b4',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.me': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais',
+  'https://api-fgmods.ddns.net': 'fg-dylux',
+  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.lolhuman.xyz': 'GataDiosV2',
+  'https://botstyle-api.herokuapp.com': 'Eyar749L',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://anabotofc.herokuapp.com/': 'AnaBot'
 } 
 
 let file = fileURLToPath(import.meta.url)
