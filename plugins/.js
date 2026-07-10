f4e1275b5fcc4eba95d2aac3a6b691517891595e
Wichi <barbozasebastian408@gmail.com>
2026-07-09 20:40:37 -0400

    Actualizar .js

--- plugins/.js
+++ plugins/.js
@@ -1,25 +1,20 @@
 let handler = async (m, { text, usedPrefix, command }) => {
     global.db.data.sticker = global.db.data.sticker || {}
+    if (!m.quoted) return m.reply('Responde a un sticker.')
+    if (!m.quoted.fileSha256) return m.reply('El archivo no es un sticker válido.')
+    if (!text) return m.reply(`Uso: ${usedPrefix + command} <nombre_comando>`)
     
-    if (!m.quoted) return m.reply('*⚠️ Responde al sticker que deseas asignar como comando.*')
-    if (!m.quoted.fileSha256) return m.reply('*⚠️ El archivo no contiene un hash válido.*')
-    if (!text) return m.reply(`*⚠️ Especifica el comando.*\n\n*Ejemplo:*\n> *${usedPrefix + command} #group-cerrar.js*`)
-
     let hash = m.quoted.fileSha256.toString('base64')
-    
     global.db.data.sticker[hash] = {
-        text: text.trim(),
+        text: text,
         mentionedJid: m.mentionedJid,
         creator: m.sender,
         at: +new Date()
     }
-
-    m.reply(`*✅ Sticker asignado correctamente al comando:* \`${text.trim()}\``)
+    m.reply(`Comando asignado correctamente al sticker: ${text}`)
 }
-
 handler.help = ['setcmd']
 handler.tags = ['database']
 handler.command = ['setcmd']
 handler.rowner = true
-
 export default handler
