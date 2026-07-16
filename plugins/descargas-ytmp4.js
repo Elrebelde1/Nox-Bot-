99deb7d188395ddb8ec5f7ac0a7f0c6024015ef8
Wichi <barbozasebastian408@gmail.com>
2026-07-16 16:22:27 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -2,7 +2,7 @@ import yts from 'yt-search'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return m.reply(`🛸 *[ BOX BOT MD ]* 🌌\n\n🚩 *Ingresa un enlace de YouTube.*`)
+  if (!text) return m.reply(`🛸 *[ NOX BOT MD ]* 🌌\n\n🚩 *Ingresa un enlace de YouTube.*`)
   
   let res = await yts(text)
   let vid = res.videos[0]
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let json = await (await fetch(apiUrl)).json()
   if (!json.status) return m.reply(`❌ *Error al procesar el video.*`)
 
-  let cap = `🛸 *[ BOX BOT MD ]* 🌌\n\n🎶 *Título:* ${vid.title}\n📁 *Formato:* MP4 (720p)\n\n⚙️ *Box Bot MD • Descargando...* 🌀`
+  let cap = `🛸 *[ NOX BOT MD ]* 🌌\n\n🎶 *Título:* ${vid.title}\n📁 *Formato:* MP4 (720p)\n\n⚙️ *Box Bot MD • Descargando...* 🌀`
 
   await conn.sendMessage(m.chat, { image: { url: vid.thumbnail }, caption: cap }, { quoted: m })
   await conn.sendMessage(m.chat, { video: { url: json.data.dl }, mimetype: 'video/mp4' }, { quoted: m })
