d248b61868dc99cb81bcd2bef4b59b2b985cc56b
Wichi <barbozasebastian408@gmail.com>
2026-07-27 13:25:50 -0400

    Actualizar main-tts.js

--- plugins/main-tts.js
+++ plugins/main-tts.js
@@ -27,6 +27,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['tts <texto>']
 handler.tags = ['tools']
-handler.command = ['tts']
+handler.command = /^g?tts|ttss$/i
 
 export default handler
